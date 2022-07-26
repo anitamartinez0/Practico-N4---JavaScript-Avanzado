3d1c3ff05016abe00489f7df5bd316a0ac37fcd7
anitamartinez0 <anapm1712@gmail.com>
2022-07-26 12:48:33 -0300

    listooo

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,4 +1,3 @@
-function cacheFunction(cb) {
     // Usa closures para crear un cache para la funcion cb.
     // la funcion que retornas debe aceptar un solo argumento e invocar a cb con ese argumento
     // cuando la funcion que hayas retornado es invocada de nuevo, deberia guardar el argumento y el resultado de la invocacion
@@ -11,26 +10,27 @@ function cacheFunction(cb) {
     // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
     // Aqui tu codigo
 
-}
+    function cacheFunction(cb) {
+
+    }
 
     // Instrucción:
     // Dada una cadena, escriba una función recursiva para devolver la cadena invertida. */// Ejemplo:
     // => 'divoco'
     //cadena inversa('covid')
     // Aqui tu codigo
-function CadenaInvertida(str) {
+    
+    function CadenaInvertida(str) {
 
         if (str === "")
           return "";
         else
           return CadenaInvertida(str.substr(1)) + str.charAt(0);
-      }
+    }
+    
+    console.log(CadenaInvertida("divoc"));
 
-console.log(CadenaInvertida("divoc"));
 
-
-
-function EnteroMasGrande(array) {
     // Instruction:
     //Dada una matriz, escriba una función recursiva para encontrar el entero más grande en una matriz. */// Ejemplos:
     //maxOf([1, 4, 5, 3])
@@ -38,8 +38,20 @@ function EnteroMasGrande(array) {
     //maxOf([3, 1, 6, 8, 2, 4, 5])
     // => 8
     //Aqui tu codigo
-}
-function Permutaciones(str){
+
+    function EnteroMasGrande(array) {
+
+        if (array.length === 1){ 
+            return array[0]
+        } else if(array[0]>array[array.length-1]){
+            array.pop()
+            return EnteroMasGrande(array) 
+            } else {
+            return EnteroMasGrande(array.slice(1) )
+        }
+    }
+    
+    console.log(EnteroMasGrande([1, 4, 5, 3]))
     
 // Instrucción:
 //Dada una cadena, escriba una función recursiva para imprimir una matriz de todas las permutaciones posibles de la cadena. */// Ejemplos:
@@ -48,4 +60,35 @@ function Permutaciones(str){
 //permutaciones('aabc')
 // => ["aabc", "aacb", "abac", "abca", "acab", "acba", "baac", "baca", "bcaa", "caab", "caba", " cbaa"]
 //Aqui tu codigo
+
+function Permutaciones(str){
+    var nuevo = [];
+
+    if(str.length === 1){
+        nuevo.push(str)
+        return nuevo
+    }
+
+    for(var i=0; i<str.length; i++){
+        var primero = str[i];
+        var segundo = str.substring(0,i) + str.substring(i+1);
+        var segundo1 = Permutaciones(segundo);
+
+        for(var j=0; j<segundo1.length; j++){
+            nuevo.push(primero + segundo1[j])
+        }
+    }
+    return nuevo;    
 }
+console.log(Permutaciones('abc'));
+
+ //   void permutaion(String str, String prefix){ 
+   //     if(str.length() == 0){ 
+   //         System.out.println(prefix); 
+     //   } else{ 
+       //     for(int i=0; i < str.length(); i++){ 
+         //       string rem = str.substring(0,i) + str.substring(i+1); 
+           //     permutaion(rem, prefix + str.charAt(i)); 
+            //} 
+        //} 
+    //}
